7006a7e7ec8564f36eef9d37cf3184a9c3edabbc
Alvin Quijano <alvin.quijano@live.ca>
2019-09-26 14:15:27 -0400

    Created RecipeList Model

---
+++ MyRecipe/Models/RecipeList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRecipe.Models
+{
+    /// <summary>
+    /// 
+    /// This is the RecipeList Model
+    /// It creates a list of recipes and returns it to the RecipeList View 
+    /// 
+    /// </summary>
+    public static class RecipeList
+    {
+        private static List<Recipe> recipes = new List<Recipe>();
+
+        public static IEnumerable<Recipe> Recipes
+        {
+            get
+            {
+                return Recipes;
+            }
+        }
+    }
+}
