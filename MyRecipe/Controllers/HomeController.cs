2e338ef6751534ec1ce0fda4c84d18b03e82e1b6
acqno <alvin.quijano@live.ca>
2019-09-26 01:49:41 -0400

    Initial commit

---
+++ MyRecipe/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyRecipe.Models;
+
+namespace MyRecipe.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Action method that calls the index (homepage) view 
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyRecipe/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRecipe.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+
+    }
+}
